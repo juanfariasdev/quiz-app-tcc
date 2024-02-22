de18fc19930976d1aaa1be0bc6704e7019c015e7
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-02-21 22:44:29 -0300

    styled login

--- app/(public)/login.tsx
+++ app/(public)/login.tsx
@@ -1,19 +1,18 @@
-import { router } from 'expo-router';
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { Button } from '~/components/button';
 import { Input } from '~/components/input';
 
 const LoginPage = () => {
   return (
-    <View className="h-full justify-center px-3 py-3">
-      <View className="flex-1 justify-center w-full items-center py-8">
-        <Text className="text-3xl font-bold pt-2 text-center">Bem-vindo novamente 👋</Text>
+    <View className="h-full px-6 py-3">
+      <View className="flex-1  w-full py-8">
+        <Text className="text-3xl font-bold pt-2">Bem-vindo novamente 👋</Text>
 
         <View className="w-full gap-8 pt-10">
-          <Input title="Email" placeholder="Digite seu email!" />
+          <Input title="Email" placeholder="exemplo@email.com" />
 
-          <Input title="Senha" placeholder="Digite sua senha!" type="password" />
+          <Input title="Senha" placeholder="********" type="password" />
         </View>
       </View>
       <Button title="Logar" layout="default" />
